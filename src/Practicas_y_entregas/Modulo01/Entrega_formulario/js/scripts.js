3607037ff76a099f3b2d1e1cbfb4876e0855a7cf
MercedesNavarro <m.navarroc@live.com>
2020-08-03 22:37:47 +0200

    Module 01 practice. Updating a form with JS

---
+++ src/Practicas_y_entregas/Modulo01/Entrega_formulario/js/scripts.js
@@ -0,0 +1,27 @@
+var image,
+    firstName,
+    lastName,
+    update,
+    refresh;
+
+image = document.getElementById("image");
+firstName = document.getElementById("first-name");
+lastName = document.getElementById("last-name");
+update = document.getElementById("update");
+refresh = document.getElementById("refresh");
+
+update.addEventListener("click", function(){
+    image.src = "https://www.pngitem.com/pimgs/m/22-223925_female-avatar-female-avatar-no-face-hd-png.png";
+    firstName.value = "Merce";
+    lastName.value = "Navarro";
+
+});
+
+refresh.addEventListener("click", function(){
+    image.src = "";
+    firstName.value = "";
+    lastName.value = "";
+
+});
+
+
